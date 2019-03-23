261780d6fb21321cd9dbc9fd1690c214d5396b79
Devil0F <40351237+Devil0F@users.noreply.github.com>
2019-03-23 15:19:06 +0800

    冒泡排序法

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -5,7 +5,7 @@ def Bubble_sort(arr):
 	for i in range(0,n-1):
 		sign = False
 		for j in range(0,n-1-i):
-			if arr[j]>arr[j+1]:
+			if arr[j] > arr[j+1]:
 				temp = arr[j]
 				arr[j] = arr[j+1]
 				arr[j+1] = temp 
