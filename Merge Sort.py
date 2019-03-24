9d1f07d2b8a06c1b9f25ae4c9a421e187bfdb937
Devil0F <40351237+Devil0F@users.noreply.github.com>
2019-03-24 17:02:58 +0800

    归并排序法

--- Merge Sort.py
+++ Merge Sort.py
@@ -1,4 +1,4 @@
-def Merge_sort(arr,st,n):
+def Merge_sort(arr,st,n):            #arr也随着每次调用发生变化
 	if n-1<=st:
 		return arr
 	arr_l = arr[:((n-1)/2)+1]    #切片末端不包含，故此处+1
@@ -22,7 +22,7 @@ def Merge(arr,arr_l,arr_r):
 			temp[k] = arr_l[i]
 			k = k+1
 			i = i+1
-	if i == len(arr_l):
+	if i == len(arr_l):				#将剩余数据拷贝给temp
 		temp[k:] = arr_r[j:]
 	else:
 		temp[k:] = arr_l[i:]
