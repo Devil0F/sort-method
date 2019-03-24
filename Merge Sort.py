7e36f3a3b9f01c7548e856c9869d200b6351fe91
Devil0F <40351237+Devil0F@users.noreply.github.com>
2019-03-24 22:47:13 +0800

    Update Merge Sort.py
    一些修改，节省了一些空间，删除了一些多余的代码

--- Merge Sort.py
+++ Merge Sort.py
@@ -5,30 +5,28 @@ def Merge_sort(arr,st,n):            #arr也随着每次调用发生变化
 	arr_r = arr[((n-1)/2)+1:]
 	Merge_sort(arr_l,0,len(arr_l))
 	Merge_sort(arr_r,0,len(arr_r))
-	arr = Merge(arr,arr_l,arr_r)
+	Merge(arr,arr_l,arr_r)
 	return arr
 	
 	
 	
 def Merge(arr,arr_l,arr_r):
 	i,j,k= 0,0,0
-	temp = arr
 	while i != len(arr_l) and j != len(arr_r):
 		if arr_l[i] > arr_r[j]:
-			temp[k] = arr_r[j]
+			arr[k] = arr_r[j]
 			k = k+1
 			j = j+1
 		else:
-			temp[k] = arr_l[i]
+			arr[k] = arr_l[i]
 			k = k+1
 			i = i+1
 	if i == len(arr_l):				#将剩余数据拷贝给temp
-		temp[k:] = arr_r[j:]
+		arr[k:] = arr_r[j:]
 	else:
-		temp[k:] = arr_l[i:]
-	arr = temp
-	return arr
+		arr[k:] = arr_l[i:]
+
 	
 arr = [6,4,8,1,4,2,7,6]
-arr=Merge_sort(arr,0,len(arr))
+Merge_sort(arr,0,len(arr))
 print(arr)
